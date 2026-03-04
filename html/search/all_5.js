227f71aea464ae9e447c252829ffe1d6d00fdb17
Doxygen CI <ci-arduino@invalid>
2026-03-04 16:44:04 +0000

    Deploy docs to GitHub Pages from commit 3efd3f7348
    Commit: 3efd3f7348c3cbd9e3e52ca0db7184d56fd30a66
    GitHub Actions run: 22679142008

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -5,6 +5,7 @@ var searchData=
   ['readmagneticy',['readMagneticY',['../class_adafruit___t_m_a_g5273.html#a01ae5b3e7eb4083de4cb545430cbafa6',1,'Adafruit_TMAG5273']]],
   ['readmagneticz',['readMagneticZ',['../class_adafruit___t_m_a_g5273.html#a3747f804644e987319deda4bfd1880ee',1,'Adafruit_TMAG5273']]],
   ['readmagnitude',['readMagnitude',['../class_adafruit___t_m_a_g5273.html#a35711e1c3bc589a92f83d4a853965e7f',1,'Adafruit_TMAG5273']]],
+  ['readmagnitudemt',['readMagnitudeMT',['../class_adafruit___t_m_a_g5273.html#a88dea74cc8850576b7486d1c295c4863',1,'Adafruit_TMAG5273']]],
   ['readx',['readX',['../class_adafruit___t_m_a_g5273.html#a1313613e90aa9783703246bbf7f92eec',1,'Adafruit_TMAG5273']]],
   ['ready',['readY',['../class_adafruit___t_m_a_g5273.html#aee010b809cf05e15129fab0c7acb767e',1,'Adafruit_TMAG5273']]],
   ['readz',['readZ',['../class_adafruit___t_m_a_g5273.html#aec88d2ef40bb8993abb9ed69f369be66',1,'Adafruit_TMAG5273']]]
